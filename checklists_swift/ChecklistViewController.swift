4427cbb8507c3b174cfd31793555515d772482c1
Fadie Hannona <fadiehannona@gmail.com>
2015-01-20 16:29:41 +0000

    first commit

---
+++ checklists_swift/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  checklists_swift
+//
+//  Created by Fadie on 20/01/2015.
+//  Copyright (c) 2015 Fadie. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Checklist
+### Using Swift
+
+#### Learning outcomes
+
+- To fully understand using TableViews and Navigation Controllers
+- To understand how to recycle cells in a TableView to display data.
+
+#### Technologies Used
+
+- Xcode
+- Swift
+
